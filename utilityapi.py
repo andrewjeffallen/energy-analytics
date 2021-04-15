de263e837f756d0fc9e0505165be39f74966af94
andrew allen <59720906+andrewjeffallen@users.noreply.github.com>
2021-04-14 21:21:12 -0700

    Update utilityapi.py

--- utilityapi.py
+++ utilityapi.py
@@ -1,16 +1,3 @@
-import numpy 
-import pandas as pd
-import os
-import json
-import boto3
-import io
-import gzip
-import sys
-import requests
-import time
-
-from datetime import date
-
 import requests
 import json
 import time
@@ -21,6 +8,7 @@ import pandas as pd
 def get_authorization_list():
     authorization_list = []
     url ='https://utilityapi.com/api/v2/authorizations'
+    print(url)
     headers = {
         'Authorization': 'Bearer 2793bc2c7aeb4013bf817f656213e056',
         'Content-Type': 'application/json'
@@ -48,9 +36,11 @@ def get_meter_info_from_authorization(authorization_uid):
 
 def get_meter_list_from_auth(authorization_uid, **context):
     print(f"Meters for Authorization {authorization_uid}:")
-    for i in range(-1,len(get_meter_info_from_authorization(157638)['meters'])):
-            print(get_meter_info_from_authorization(157638)['meters'][i]['uid'])
-            
+    meter_list=[]
+    for i in range(-1,len(get_meter_info_from_authorization(authorization_uid)['meters'])):
+        meter_list.append(get_meter_info_from_authorization(authorization_uid)['meters'][i]['uid'])
+        print(get_meter_info_from_authorization(authorization_uid)['meters'][i]['uid'])
+    return pd.DataFrame(meter_list)
             
             
 #Collect past intervals from the meter
@@ -93,20 +83,3 @@ def send_csv_gz_to_s3(meter_uid):
     return return_code
 
 
-if __name__ == "__main__":
-    
-    """
-    Function to write data from Utility-API to AWS S3 
-    Arguments: meter_uid, util_data
-    meter_uid (int)
-    """
-
-    meter_uid = sys.argv[1]
-    
-
-    return_code = send_csv_gz_to_s3(meter_uid)
-
-    if return_code == 0:
-        sys.exit(0)
-    else:
-        raise SystemError(f"Error {return_code}")
