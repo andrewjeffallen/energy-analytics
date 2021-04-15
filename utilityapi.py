deccf2fa73a49edc85c7ee98c544dafbb469aded
andrew allen <59720906+andrewjeffallen@users.noreply.github.com>
2021-04-14 20:44:47 -0700

    Update utilityapi.py

--- utilityapi.py
+++ utilityapi.py
@@ -1,33 +1,61 @@
-import requests, json, time 
+import numpy 
+import pandas as pd
+import os
+import json
+import boto3
+import io
+import gzip
+import sys
+import requests
+import time
+
+from datetime import date
+
+import requests
+import json
+import time
+import numpy as np
+import pandas as pd
 
 
-# Step 1 is to create a new, blank form.
-def call1():
-    url = 'https://utilityapi.com/api/v2/forms'
-    print(url)
-    headers = {'Authorization': 'Bearer 2793bc2c7aeb4013bf817f656213e056'}
-
-    r = requests.post(url, headers=headers)
-    return json.loads(r.text)
-
-# Step 2: once blank form is created, Simulate Someone submitting. Receive
-def call2(uid):
-    url = 'https://utilityapi.com/api/v2/forms/' + str(uid) + '/test-submit'
-    print(url)
-    payload = {
-        "utility": "DEMO",
-        "scenario": "residential"
-    }
+def get_authorization_list():
+    authorization_list = []
+    url ='https://utilityapi.com/api/v2/authorizations'
     headers = {
         'Authorization': 'Bearer 2793bc2c7aeb4013bf817f656213e056',
         'Content-Type': 'application/json'
     }
-    r = requests.post(url, data=json.dumps(payload), headers=headers)
+    r = requests.get(url, headers=headers)
+    json.loads(r.text)['authorizations']
+    
+    for i in range(-1,len(json.loads(r.text)['authorizations'])-1):
+        authorization_list.append(json.loads(r.text)['authorizations'][i]['uid'])
+
+    return authorization_list
+
+
+
+def get_meter_info_from_authorization(authorization_uid):
+    url = f'https://utilityapi.com/api/v2/meters?authorizations={authorization_uid}'
+    headers = {
+        'Authorization': 'Bearer 2793bc2c7aeb4013bf817f656213e056',
+        'Content-Type': 'application/json'
+    }
+    r = requests.get(url, headers=headers)
     return json.loads(r.text)
 
-# Get Authorizations and Meters associated with the referral code(includes meter_uid 44445555)
-def call3(referral):
-    url = 'https://utilityapi.com/api/v2/authorizations?referrals=' + str(referral) + '&include=meters'
+
+
+def get_meter_list_from_auth(authorization_uid, **context):
+    print(f"Meters for Authorization {authorization_uid}:")
+    for i in range(-1,len(get_meter_info_from_authorization(157638)['meters'])):
+            print(get_meter_info_from_authorization(157638)['meters'][i]['uid'])
+            
+            
+            
+#Collect past intervals from the meter
+def get_intervals(meter_uid):
+    url = f'https://utilityapi.com/api/v2/intervals?meters={meter_uid}'
     print(url)
     headers = {
         'Authorization': 'Bearer 2793bc2c7aeb4013bf817f656213e056',
@@ -37,40 +65,48 @@ def call3(referral):
     return json.loads(r.text)
 
 
-# Activate the meter to collect historical data
-def call4(meterid):
-    url = 'https://utilityapi.com/api/v2/meters/historical-collection'
-    print(url)
-    payload = {
-            "meters": [str(meterid)]
-        }
-    headers = {
-        'Authorization': 'Bearer 2793bc2c7aeb4013bf817f656213e056',
-        'Content-Type': 'application/json'
-    }
-    r = requests.post(url, data=json.dumps(payload), headers=headers)
-    return json.loads(r.text)
+def send_csv_gz_to_s3(meter_uid):
+    
+    json_dict =get_intervals(meter_uid=meter_uid)['intervals']
+    df= pd.concat([pd.DataFrame(json_dict), pd.DataFrame(list(json_dict[0]['readings']))], axis=1).drop('readings', 1)
+    
+    load_date = date.today().strftime("%Y-%m-%d")
+    
+    session = boto3.session.Session(profile_name="default", )
+    s3_client = session.client("s3", use_ssl=False)    
 
-#Poll the meter to check whether we can collect our bill from it or not
-def call5(meterid):
-    url = 'https://utilityapi.com/api/v2/meters/'+meterid
-    print(url)
-    headers = {
-        'Authorization': 'Bearer 2793bc2c7aeb4013bf817f656213e056',
-        'Content-Type': 'application/json'
-    }
-    r = requests.get(url, headers=headers)
-    return json.loads(r.text)
+    csv_buffer = io.StringIO()
+    
+    df.to_csv(csv_buffer, index=False)
+    csv_buffer.seek(0)
+    gz_buffer = io.BytesIO()
+    
 
-#Collect past bills from the meter
-def call6(meterid):
-    url = 'https://utilityapi.com/api/v2/bills?meters='+meterid
-    print(url)
-    headers = {
-        'Authorization': 'Bearer 2793bc2c7aeb4013bf817f656213e056',
-        'Content-Type': 'application/json'
-    }
-    r = requests.get(url, headers=headers)
-    return json.loads(r.text)
+    with gzip.GzipFile(mode='w', fileobj=gz_buffer) as gz_file:
+        gz_file.write(bytes(csv_buffer.getvalue(), 'utf-8'))
+    try:
+        s3_client.put_object(Bucket='utility-api-raw', Key=f"""intervals/{meter_uid}/{load_date}.csv.gz""", Body=gz_buffer.getvalue())
+        return_code = 0
+    except Exception as e:
+        return_code = 1
+        print(e)
+    return return_code
 
-main()
+
+if __name__ == "__main__":
+    
+    """
+    Function to write data from Utility-API to AWS S3 
+    Arguments: meter_uid, util_data
+    meter_uid (int)
+    """
+
+    meter_uid = sys.argv[1]
+    
+
+    return_code = send_csv_gz_to_s3(meter_uid)
+
+    if return_code == 0:
+        sys.exit(0)
+    else:
+        raise SystemError(f"Error {return_code}")
