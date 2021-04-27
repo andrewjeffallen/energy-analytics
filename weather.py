9f10a48e881294e45bbcd34a5848192991957c15
andrew allen <59720906+andrewjeffallen@users.noreply.github.com>
2021-04-27 12:54:38 -0700

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,25 @@
+import requests
+import json
+from geopy.geocoders import Nominatim
+
+class Weather:
+
+  def __init__(self, locale):
+    self.geolocator = Nominatim(user_agent="python")
+    self.location = self.geolocator.geocode(str(locale))
+    self.lat = self.location.latitude
+    self.lon = self.location.longitude
+    self.url = "https://api.weather.gov/points/" + str(self.lat) + "," + str(self.lon)
+
+  def getWeather(self):
+    forecast_url = json.loads(requests.get(self.url).content)["properties"]["forecastHourly"]
+    res = json.loads(requests.get(forecast_url).content)
+    print(self.location)
+    print("Current temperature is: " + str(res["properties"]["periods"][0]["temperature"]) + "°F")
+    print("Windspeed: " + str(res["properties"]["periods"][0]["windSpeed"]))
+    print("Current conditions: " + str(res["properties"]["periods"][0]["shortForecast"]))
+
+if __name__ == "__main__":
+  locale = input("\r\nZipcode, City/State: " )
+  weather = Weather(locale)
+  weather.getWeather()
