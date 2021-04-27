7b29abffe5bfacfeac9ba58cf517d750c063a0b5
andrew allen <59720906+andrewjeffallen@users.noreply.github.com>
2021-04-27 13:26:37 -0700

    Update utilityapi.py

--- utilityapi.py
+++ utilityapi.py
@@ -3,16 +3,27 @@ import json
 import time
 import numpy as np
 import pandas as pd
+import boto3
 
+from pandas.io.json import json_normalize
 
-INSERTTOKENHERE=' your utiltiy-api token'
+import numpy 
+import pandas as pd
+import os
+import json
+import boto3
+import io
+import gzip
+from geopy.geocoders import Nominatim
+import sys
+
+from datetime import date
 
 def get_authorization_list():
     authorization_list = []
     url ='https://utilityapi.com/api/v2/authorizations'
-    print(url)
     headers = {
-        'Authorization': 'Bearer INSERTTOKENHERE',
+        'Authorization': 'Bearer 2793bc2c7aeb4013bf817f656213e056',
         'Content-Type': 'application/json'
     }
     r = requests.get(url, headers=headers)
@@ -28,7 +39,7 @@ def get_authorization_list():
 def get_meter_info_from_authorization(authorization_uid):
     url = f'https://utilityapi.com/api/v2/meters?authorizations={authorization_uid}'
     headers = {
-        'Authorization': 'Bearer INSERTTOKENHERE',
+        'Authorization': 'Bearer 2793bc2c7aeb4013bf817f656213e056',
         'Content-Type': 'application/json'
     }
     r = requests.get(url, headers=headers)
@@ -37,7 +48,6 @@ def get_meter_info_from_authorization(authorization_uid):
 
 
 def get_meter_list_from_auth(authorization_uid, **context):
-    print(f"Meters for Authorization {authorization_uid}:")
     meter_list=[]
     for i in range(-1,len(get_meter_info_from_authorization(authorization_uid)['meters'])):
         meter_list.append(get_meter_info_from_authorization(authorization_uid)['meters'][i]['uid'])
@@ -48,23 +58,34 @@ def get_meter_list_from_auth(authorization_uid, **context):
 #Collect past intervals from the meter
 def get_intervals(meter_uid):
     url = f'https://utilityapi.com/api/v2/intervals?meters={meter_uid}'
-    print(url)
     headers = {
-        'Authorization': 'Bearer INSERTTOKENHERE',
+        'Authorization': 'Bearer 2793bc2c7aeb4013bf817f656213e056',
         'Content-Type': 'application/json'
     }
     r = requests.get(url, headers=headers)
     return json.loads(r.text)
 
 
-def send_csv_gz_to_s3(meter_uid):
+def send_intervals_to_s3(meter_uid):
     
+    raw_int=get_intervals(meter_uid=meter_uid)
     json_dict =get_intervals(meter_uid=meter_uid)['intervals']
-    df= pd.concat([pd.DataFrame(json_dict), pd.DataFrame(list(json_dict[0]['readings']))], axis=1).drop('readings', 1)
+
+
+    flattened_df= pd.concat([pd.DataFrame(json_dict), pd.DataFrame(list(json_dict[0]['readings']))], axis=1).drop('readings', 1)
+
+    intervals = flattened_df[['start','end','kwh']]
+    intervals.loc[:,'meter_uid'] = flattened_df['meter_uid'][1]
+    intervals.loc[:,'authorization_uid'] = flattened_df['authorization_uid'][1]
+    intervals.loc[:,'utility'] = flattened_df['utility'][1]
+    intervals.loc[:,'service_address'] = list(flattened_df['base'])[0]['service_address']
+    
+    df = intervals
+    print(f'Loading {len(df)} Rows to S3 for meter_uid {meter_uid}')
     
     load_date = date.today().strftime("%Y-%m-%d")
     
-    session = boto3.session.Session(profile_name="default", )
+    session = boto3.session.Session(profile_name="data-arch", )
     s3_client = session.client("s3", use_ssl=False)    
 
     csv_buffer = io.StringIO()
@@ -77,11 +98,22 @@ def send_csv_gz_to_s3(meter_uid):
     with gzip.GzipFile(mode='w', fileobj=gz_buffer) as gz_file:
         gz_file.write(bytes(csv_buffer.getvalue(), 'utf-8'))
     try:
-        s3_client.put_object(Bucket='utility-api-raw', Key=f"""intervals/{meter_uid}/{load_date}.csv.gz""", Body=gz_buffer.getvalue())
+        s3_client.put_object(Bucket='utility-api', Key=f"""intervals/{meter_uid}/{load_date}.csv.gz""", Body=gz_buffer.getvalue())
         return_code = 0
     except Exception as e:
         return_code = 1
         print(e)
     return return_code
 
+meter_list = [ 
+    725797, 
+    725798, 
+    725799,
+    725801
+]
+
+def main():
+    for uid in meter_list:
+        send_intervals_to_s3(uid)
+        
 
