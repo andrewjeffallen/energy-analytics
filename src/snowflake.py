b730a5d72d861c36ec20b3a0ece414f5cf04a6f9
andrew allen <59720906+andrewjeffallen@users.noreply.github.com>
2021-05-18 13:28:50 -0600

    Update snowflake.py

--- src/snowflake.py
+++ src/snowflake.py
@@ -1,39 +1,49 @@
-from snowflake import connector
-import boto3
-from aws import get_secret, get_aws_session
+def snowflake_connection(schema):
+    con = snowflake.connector.connect('credentials')
+    return con
 
-def snowflake_connection(secret_name):
-  
-  snowflake_credentials = get_secret(f"{secret_name}")
+def load_data_to_snowflake(schema,table,external_stage,utility_file):
+    load_date = date.today().strftime("%Y-%m-%d")
+    print("")
+    print(f"loading {utility_file} data for files ingested on {load_date}")
+    print("")
+    con=snowflake_connection(schema)
+    cs=con.cursor()
+    print(f"Truncating {schema}.{table} Prior to Load")
+    cs.execute(f"""truncate {schema}.{table};""")
+    print(f"Succesfully Truncated {schema}.{table} Prior to load")
+    print("")
+    print(f"Loading {schema}.{table} ")
+    
+    if utility_file=='bills':
+        utility_file_length=16
+    elif utility_file=='intervals':
+        utility_file_length=13
+    
+    cols =", ".join([f"${i}" for i in range(1,utility_file_length+1)])
+    
+    query = f"""COPY INTO {schema}.{table}
+                FROM (
+                SELECT {cols}
+                FROM @{external_stage}/{utility_file}/{load_date}/)
+                ENFORCE_LENGTH = True
+                    FILE_FORMAT = (
+                    ERROR_ON_COLUMN_COUNT_MISMATCH=FALSE
+                    type = csv 
+                    FIELD_DELIMITER = ','
+                    COMPRESSION = AUTO
+                    SKIP_HEADER = 1
+                    FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+                    EMPTY_FIELD_AS_NULL = True
+                    NULL_IF = ('NULL','null','','None')
+                    VALIDATE_UTF8 = False
+                    )
+                    ;
 
-  con = snowflake.connector.connect(
-                user=sf_user,
-                password=snowflake_credentials.get("password"),
-                account=snowflake_credentials.get("account"),
-                warehouse=snowflake_credentials.get("wh"),
-                database=snowflake_credentials.get("sfdb"),
-                schema=snowflake_credentials.get("schema")
-                )
-  return con
+                """
 
-def load_snowflake(secret_name,table_name):
-  con=snowflake(secret_name)
-  with con.cursor() as cur:
-      cur.execute(f"""COPY INTO STAGE.{table_name} 
-                  FROM (
-                  SELECT *
-                  FROM @EXT_STAGE_NM/{table_name}/
-                  ENFORCE_LENGTH = True
-                      FILE_FORMAT = (
-                      type = csv 
-                      FIELD_DELIMITER = ','
-                      COMPRESSION = AUTO
-                      SKIP_HEADER = 1
-                      FIELD_OPTIONALLY_ENCLOSED_BY = '"'
-                      EMPTY_FIELD_AS_NULL = True
-                      NULL_IF = ('NULL','null','')
-                      VALIDATE_UTF8 = False
-                      )
-
-                      Pattern = '.*.csv'; """)
-      
+    results=cs.execute(query)
+    rows=cs.fetchall()
+    df=pd.DataFrame(rows, columns= [desc[0] for desc in cs.description])
+    
+    print(f"Succesfully loaded {utility_file} data into {schema}.{table} with {len(df)} files ")
